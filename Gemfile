14d8b7071cbf2fe74ec6f72d5528e444b36eaa9e
squarebe <squarebe03@gmail.com>
2015-08-16 12:01:24 +0100

    initial commit added some presettings

--- Gemfile
+++ Gemfile
@@ -1,11 +1,19 @@
 source 'https://rubygems.org'
 ruby '2.2.2'
 
+gem "sinatra"
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'rake'
+
 group :test do
   gem 'rspec'
   gem 'cucumber'
   gem 'rubocop-rspec'
   gem 'rubocop'
   gem 'coveralls', require: false
+  gem 'capybara'
+  gem 'rspec-sinatra'
+  gem 'database_cleaner' 
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,28 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app/chitter.rb')
+
 require 'coveralls'
 require 'simplecov'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'database_cleaner'
+
+Capybara.app = Chitter
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
